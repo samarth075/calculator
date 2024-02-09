b35e06f3fd1dfd8e9e5433b848a15b18a4a8e0b2
01fe22bca075 <01fe22bca075@kletech.ac.in>
2024-02-09 16:20:40 +0530

    program on math

---
+++ math_functions.java
@@ -0,0 +1,15 @@
+public class math_functions {
+    public static void main(String args[])
+    {
+        System.out.println("Maximum of a number = "+Math.max(5,3));
+        System.out.println("MInimum of a number = "+Math.min(5,3));
+        System.out.println("Square root of a number = "+Math.sqrt(5));
+        System.out.println("Maximum of a number = "+Math.cbrt(5));
+        System.out.println("Maximum of a number = "+Math.pow(5,3));
+        System.out.println("Maximum of a number = "+Math.random());
+
+
+
+    }
+    
+}
